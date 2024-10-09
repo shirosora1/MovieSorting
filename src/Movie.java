5a3f0370a78e45c8a39e03baf2b6fc23f648eb46
shirosora1 <13071798+shirosora1@users.noreply.github.com>
2024-10-09 15:22:34 +0100

    Added variables and constructor to Movie, added a List in Main

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,13 @@
 public class Movie {
+    String title;
+    int releaseYear;
+    double ratingImdb;
+    int ageRating;
+
+    public Movie(String title, int releaseYear, double ratingImdb, int ageRating) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.ageRating = ageRating;
+        this.ratingImdb = ratingImdb;
+    }
 }
